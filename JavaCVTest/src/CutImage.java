466faa2fa112eadc41b24e501a1be2a545cd1d8d
supervitis <supervitis@supervitis-pc>
2014-08-06 03:44:43 +0200

    Any size of image, any number of pieces

--- JavaCVTest/src/CutImage.java
+++ JavaCVTest/src/CutImage.java
@@ -15,33 +15,34 @@ public class CutImage {
 
 		IplImage orig = cvLoadImage("lena.jpg");
 		// Creating rectangle by which bounds image will be cropped
+		int lines = 4;
+		int heightTemp = orig.height() / lines;
+		int columns = 3;
+		int widthTemp = orig.width() / columns;
 
+		for (int i = 0; i < lines; i++) {
+			for (int j = 0; j < columns; j++) {
 
-		for (int i = 0; i < 4; i++) {
+				CvRect r = new CvRect();
 
-			CvRect r = new CvRect();
-			if (i < 2)
-				r.x(0);
-			else
-				r.x(256);
-			if (i == 1 || i == 3)
-				r.y(256);
-			else
-				r.y(0);
-			r.width(256);
-			r.height(256);
+				r.x(j * widthTemp);
+				r.y(i * heightTemp);
+				r.width(widthTemp);
+				r.height(heightTemp);
 
-			// After setting ROI (Region-Of-Interest) all processing will only
-			// be done on the ROI
-			cvSetImageROI(orig, r);
-			IplImage cropped = cvCreateImage(cvGetSize(orig), orig.depth(),
-					orig.nChannels());
-			// Copy original image (only ROI) to the cropped image
-			cvCopy(orig, cropped);
-			String temp = "cropped" + i + ".jpg";
-			cvSaveImage(temp, cropped);
-			Lena.demo("lena.jpg", temp);
-			System.out.println("Iteration " + i);
+				// After setting ROI (Region-Of-Interest) all processing will
+				// only
+				// be done on the ROI
+				cvSetImageROI(orig, r);
+				IplImage cropped = cvCreateImage(cvGetSize(orig), orig.depth(),
+						orig.nChannels());
+				// Copy original image (only ROI) to the cropped image
+				cvCopy(orig, cropped);
+				String temp = "cropped" + i + ".jpg";
+				cvSaveImage(temp, cropped);
+				Lena.demo("lena.jpg", temp);
+				System.out.println("Iteration " + i);
+			}
 		}
 
 	}
